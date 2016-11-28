09d7ff35427f75a814b577d3efcb7dcaea5b6a86
Pedro Syd <pedro.saadi.aquino@gmail.com>
2016-11-28 16:34:26 -0200

    Controlling search-bar

--- udemy-course/WeatherApp/src/components/app.js
+++ udemy-course/WeatherApp/src/components/app.js
@@ -4,7 +4,6 @@ import SearchBar from '../containers/search-bar'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
       <SearchBar />
     );
   }
--- udemy-course/WeatherApp/src/containers/search-bar.js
+++ udemy-course/WeatherApp/src/containers/search-bar.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+    // Not necessary because I used arrow function below.
+    // Just leaving here for reference.
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e){
+    e.preventDefault();
+    this.setState({term: e.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="New York"
+          onChange={(e) => this.onInputChange(e)}
+          className="form-control"
+          value={this.state.term} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
