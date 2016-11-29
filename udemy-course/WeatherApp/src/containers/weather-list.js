5eda34721ad1dc15fbba4ce3b59a157eab8c5262
Pedro Syd <pedro.saadi.aquino@gmail.com>
2016-11-29 18:13:22 -0200

    Finished Weather Table

--- udemy-course/WeatherApp/src/containers/weather-list.js
+++ udemy-course/WeatherApp/src/containers/weather-list.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine  } from 'react-sparklines';
+
 
 class WeatherList extends Component {
-  // constructor() {
-  //
-  // }
+
+  renderChart(list, attr, opt={}) {
+    opt = {
+      color: opt.color || 'blue',
+      limit: opt.limit || 40,
+      width: opt.width || 180,
+      height: opt.height || 120,
+      margin: opt.margin || 5
+    }
+
+
+    let mapData = list.map((day) => {
+      return day.main[attr];
+    });
+
+    return (
+      <Sparklines data={mapData} limit={opt.limit}
+                  width={opt.width} height={opt.height}
+                  margin={opt.margin}>
+        <SparklinesLine color={opt.color} />
+      </Sparklines>
+    );
+  }
+
+  renderWeatherRow(cityData){
+    console.log('render', cityData);
+    return (
+      <tr key={cityData.city.id}>
+        <td>{cityData.city.name}</td>
+        <td>{this.renderChart(cityData.list, 'temp' )}</td>
+        <td>{this.renderChart(cityData.list, 'humidity')}</td>
+        <td>{this.renderChart(cityData.list, 'pressure')}</td>
+      </tr>
+    );
+  }
 
   render(){
     console.log(this.props.weather);
     let city = this.props.weather.length > 0 ? this.props.weather[0].city.name : 'Waiting';
     return (
-      <table>
+      <table className="table table-hover">
         <thead>
           <tr>
             <th>{city}</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(
+            (cityData) => this.renderWeatherRow(cityData)
+          )}
         </tbody>
       </table>
     );
