7e57555004099ebad68118280f091be8534e043e
Pedro Syd <pedro.saadi.aquino@gmail.com>
2016-11-30 02:06:07 -0200

    Fixing Chart Component

--- udemy-course/WeatherApp/src/components/chart.js
+++ udemy-course/WeatherApp/src/components/chart.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Sparklines, SparklinesLine  } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  return _.round( _.sum(data) / data.length );
+}
 
 export default function Chart(props) {
   let opt = props.opt || {};
-
+  console.log(props.dataList)
   opt = {
     color:  opt.color || 'blue',
     limit:  opt.limit || 40,
     width:  opt.width || 180,
     height: opt.height || 120,
-    margin: opt.margin || 5
+    margin: opt.margin || 5,
+    refLine: opt.refLine || 'avg',
+    dispNum: opt.disNum || average
   }
 
   // This should go to Weather list so this graph is more reusable
@@ -17,11 +24,21 @@ export default function Chart(props) {
     return day.main[props.attr];
   });
 
+  // opt.DisNum is an option to show a reduced number from the data
+  // by default we display the avarege below the chart
+  let numberDisplay = '';
+  if (opt.dispNum) {
+    let number = opt.dispNum(mapData);
+    numberDisplay = <div>{number}</div>;
+  }
+
   return (
     <Sparklines data={mapData} limit={opt.limit}
                 width={opt.width} height={opt.height}
                 margin={opt.margin}>
       <SparklinesLine color={opt.color} />
+      {opt.refLine ? <SparklinesReferenceLine type={opt.refLine} /> : ''}
+      {numberDisplay}
     </Sparklines>
   );
 }
